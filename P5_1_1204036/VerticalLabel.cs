5a1903b89e8239b1d308138744b7ad51532b25bc
nawafnaofal <nawafnaufal1@gmail.com>
2021-12-03 23:12:37 +0700

    percobaan 1

---
+++ P5_1_1204036/VerticalLabel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_1_1204036
+{
+    [ToolboxBitmap(typeof(VerticalLabel), "P5_1_1204036.VerticalLabel.ico")]
+    public partial class VerticalLabel : Control
+    {
+        public VerticalLabel()
+        {
+            InitializeComponent();
+        }
+        private string labelText;
+        //Overriding Text Property 
+        // Adding attribute for Text Property in design time support 
+        [Category("VerticalLabel"), Description("Text is displayed in container")]
+        public override string Text
+        {
+            get
+            { return labelText; }
+            set
+                {
+                    labelText = value;
+                    Invalidate();
+                }
+        }
+
+        //Overriding OnPaint event
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        { 
+
+        float sngControlwidth;
+        float sngControlHeight;
+
+        float sngTransformX;
+        float sngTransformY;
+
+        Color labelColor = new Color();
+        Pen labelBorderPen = new Pen(labelColor, 0);
+        SolidBrush labelBackColorBrush = new SolidBrush(labelColor);
+        SolidBrush labelForeColorBrush = new SolidBrush(base.ForeColor);
+
+            if (this.DesignMode)
+                base.ResizeRedraw = true;
+
+            base.OnPaint(e);
+
+            sngControlwidth = this.Size.Width;
+            sngControlHeight = this.Size.Height;
+
+            e.Graphics.DrawRectangle(labelBorderPen, 0, 0, sngControlwidth, sngControlHeight);
+            e.Graphics.FillRectangle(labelBackColorBrush, 0, 0, sngControlwidth, sngControlHeight);
+
+            //set the translation point for the graphics object - the new (0,0) location 
+            sngTransformX = 0;
+            sngTransformY = sngControlHeight;
+
+            // translate the origin used for rotation and drawing 
+            e.Graphics.TranslateTransform(sngTransformX, sngTransformY); // (@, textwidth);
+
+            // set the rotation angle for vertical text 
+            e.Graphics.RotateTransform(270);
+
+            // draw the text on the control
+            e. Graphics.DrawString(labelText, Font, labelForeColorBrush, 0, 0);
+
+
+        }
+    }
+
+}
---
+++ P5_1_1204036_Host/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace P5_1_1204036_Host
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericTextBox1 = new P5_1_1204036.NumericTextBox();
+            this.verticalLabel1 = new P5_1_1204036.VerticalLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Extended Control : NumericTextBox";
+            // 
+            // numericTextBox1
+            // 
+            this.numericTextBox1.Location = new System.Drawing.Point(12, 45);
+            this.numericTextBox1.Name = "numericTextBox1";
+            this.numericTextBox1.Size = new System.Drawing.Size(525, 20);
+            this.numericTextBox1.TabIndex = 0;
+            // 
+            // verticalLabel1
+            // 
+            this.verticalLabel1.Location = new System.Drawing.Point(98, 124);
+            this.verticalLabel1.Name = "verticalLabel1";
+            this.verticalLabel1.Size = new System.Drawing.Size(26, 77);
+            this.verticalLabel1.TabIndex = 7;
+            this.verticalLabel1.Text = "Vertical Label";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Custom Control : Vertical Label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.verticalLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericTextBox1);
+            this.Name = "Form1";
+            this.Text = "Controls Host";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private P5_1_1204036.NumericTextBox numericTextBox1;
+        private System.Windows.Forms.Label label1;
+        private P5_1_1204036.VerticalLabel verticalLabel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
